b0af0722aa4b5060953e8cd16d33d2f7fc7af552
ShoaibJarwar <mshoaibjarwar1256>
2025-08-02 23:53:56 +0500

    Jupyter Notebook

--- Week01_01-OOP/inheritance.py
+++ Week01_01-OOP/inheritance.py
@@ -11,7 +11,7 @@ class Cat(Animal):
     
     # def speak(self):
     #     return f"{self.name} says meow"
-
+  
 cat1 = Cat("Whiskers")
 print(cat1.speak())  # Whiskers says meow
 print(cat1.says())
