564a6b854e97f5d191235119a7426d998a6193d7
ShoaibJarwar <mshoaibjarwar1256>
2025-08-07 20:20:18 +0500

    Basics

--- Week02_DataScienceEssentials/Week02_NumPy/Week02_01-Basics/00_basic_performance_optimization.py
+++ Week02_DataScienceEssentials/Week02_NumPy/Week02_01-Basics/00_basic_performance_optimization.py
@@ -5,7 +5,7 @@ size = 1_000_000
 list1 = list(range(size))
 # list1 = [1,2,3,4]
 list2 = list(range(size))
-# list2 = [5,6,7,8]
+# list2 = [5,6,7,8] 
 
 start = time.time()
 result = [x + y for x, y in zip(list1, list2)]
